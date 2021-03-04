f9ec3c8d66e64052700b4a4b205c5eca516db700
WarakornWay <way52160@hotmail.com>
2021-03-04 22:56:38 +0700

    update

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -11,14 +11,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta property="og:url"           content="https://loykrathong-tak.com/" />
-    <meta property="og:type"          content="article" />
-    <meta property="og:title"         content="คุณ{{$rec->name}} ได้ลอยกระทงออนไลน์กับ loykrathong-tak.com" />
-    <meta property="og:description"   content="ลอยกระทงออนไลน์ ตาก" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="คุณ {{$rec->name}} ได้ลอยกระทงกับ loykrathong-tak.com " />
+    <meta property="og:description"   content="i have a good day !" />
     <meta property="og:image"         content="https://loykrathong-tak.com/img/s1.jpg" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
-    <title> {{$rec->name}} ลอยกระทงออนไลน์ เมืองตาก</title>
+    <title>ลอยกระทงออนไลน์ เมืองตาก</title>
 
 
     
