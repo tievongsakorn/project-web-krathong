cd9d6076e4816ca4fba7c9096796981250fe6304
WarakornWay <way52160@hotmail.com>
2021-03-04 22:46:05 +0700

    update 2

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:url"           content="https://loykrathong-tak.com/" />
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="คุณ{{$rec->name}} ได้ลอยกระทงออนไลน์กับ loykrathong-tak.com" />
-    <meta property="og:description"   content="{{$rec->kt_wish}}" />
+    <meta property="og:description"   content="ลอยกระทงออนไลน์ ตาก" />
     <meta property="og:image"         content="https://loykrathong-tak.com/img/s1.jpg" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
