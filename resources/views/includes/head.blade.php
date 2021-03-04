b69d779c7cf54a2b9846b70ae0d3ce8fd3739c2a
WarakornWay <way52160@hotmail.com>
2021-03-04 22:40:08 +0700

    update title

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
-    <title>ลอยกระทงออนไลน์ เมืองตาก</title>
+    <title> {{$rec->name}} ลอยกระทงออนไลน์ เมืองตาก</title>
 
     <meta property="og:url"           content="https://loykrathong-tak.com/" />
     <meta property="og:type"          content="website" />
